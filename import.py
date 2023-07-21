d5fcae994fb18f1f80a71a83f7ab28f1bb61a5ba
MuteApo <1805338124@qq.com>
2023-07-21 19:23:47 +0800

    enable loadingbg compression

--- import.py
+++ import.py
@@ -17,7 +17,7 @@ suffix = {
 exts = ["png", "jpg", "jpeg"]
 
 
-def exec(ab: str):
+def exec(ab: str, tex_fmt: TextureFormat):
     env: Environment = UnityPy.load(os.path.join("loadingbg", ab))
     mod = False
     for k, v in env.container.items():
@@ -33,28 +33,33 @@ def exec(ab: str):
         for x in exts:
             src = os.path.join("loadingbg_img", f"{file}.{x}")
             if os.path.exists(src):
-                print(f"[INFO] Processing: {src}")
                 img = Image.open(src)
+                w, h = img.size
+                img = img.resize((w // 4 * 4, h // 4 * 4))
                 if sprite is not None:
                     sprite.m_Rect.width, sprite.m_Rect.height = img.size
                     sprite.m_RD.textureRect.width, sprite.m_RD.textureRect.height = img.size
                     sprite.save()
                 tex2d.m_Width, tex2d.m_Height = img.size
-                tex2d.set_image(img, target_format=TextureFormat.RGBA32)
+                tex2d.set_image(img, target_format=tex_fmt)
                 tex2d.save()
+                print(f"[INFO] Import: {src}")
                 mod = True
                 break
 
     if mod:
         path = os.path.join(outdir, ab)
         with open(path, "wb") as f:
-            f.write(env.file.save("lz4"))
+            f.write(env.file.save("original"))
 
+
+choice = input("Enable compression (Y/N): ").lower()
+tex_fmt = TextureFormat.ETC2_RGB if choice == "y" else TextureFormat.RGB24
 
 outdir = "loadingbg_out"
 check_dir(outdir)
 
-tasks = [threading.Thread(target=exec, args=(_,)) for _ in os.listdir("loadingbg")]
+tasks = [threading.Thread(target=exec, args=(_, tex_fmt)) for _ in os.listdir("loadingbg")]
 [_.start() for _ in tasks]
 [_.join() for _ in tasks]
 
